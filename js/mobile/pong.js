1372743659ca5c47cc6c709367ba6fbd409d5614
lagapollo <sylvain.laure83@gmail.com>
2012-07-20 10:29:08 +0300

    Update js/mobile/pong.js

--- js/mobile/pong.js
+++ js/mobile/pong.js
@@ -3,13 +3,13 @@
 // Wait for PhoneGap to connect with the device
 //
 var DocReady = false;
+var depla;
 document.addEventListener("deviceready",onDeviceReady,false);
 
 function onDeviceReady() {
-    pictureSource=navigator.camera.CAMERA;
-    destinationType=navigator.camera.DestinationType;
+        lancerAccelerometre();
+        depla = new deplacement();
 }
-
 
 function vibrer(){
   if(DocReady){
@@ -17,3 +17,28 @@ function vibrer(){
   }
 }
 
+
+function lancerAccelerometre(){
+    var watchID = navigator.accelerometer.watchAcceleration(
+        function(acceleration){
+            console.log("votre acceleration en y : " + acceleration.y)
+            if (acceleration.y < 0.3){
+                
+                depla.keycode = 37;
+                // Envoi à gauche
+                checkDepla(depla);
+            }
+            else{
+                if (acceleration.y > 0.7){
+                    depla.keycode = 39
+                    checkDepla(depla);
+                }
+            }'
+        }), 
+        onError, options);
+}
+
+
+var deplacement = function(){
+    keycode = 0;
+}
